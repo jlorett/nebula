15184d9b82307ccacced81e3026f1003ab53e9e8
jal <joshlorett@gmail.com>
2020-01-15 18:59:02 -0500

    Update tests for TodayViewModel to account for loading fix.

--- app/src/test/java/com/joshualorett/nebula/today/TodayViewModelTest.kt
+++ app/src/test/java/com/joshualorett/nebula/today/TodayViewModelTest.kt
@@ -6,6 +6,8 @@ import com.joshualorett.nebula.apod.Apod
 import com.joshualorett.nebula.apod.ApodDataSource
 import com.joshualorett.nebula.apod.ApodRepository
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.runBlockingTest
 import okhttp3.ResponseBody.Companion.toResponseBody
@@ -42,6 +44,10 @@ class TodayViewModelTest {
         "https://example.com/hd")
         val mockDataSource = mock(ApodDataSource::class.java)
         `when`(mockDataSource.getApod(testDate)).thenReturn(flowOf(Response.success(mockApod)))
+        `when`(mockDataSource.getApod(testDate)).thenReturn(flow {
+            delay(10)
+            Response.success(mockApod)
+        })
         val apodRepo = ApodRepository(mockDataSource)
         viewModel = TodayViewModel.TodayViewModelFactory(apodRepo, testDate).create(TodayViewModel::class.java)
         assertTrue(viewModel.loading.value == true)
@@ -60,11 +66,32 @@ class TodayViewModelTest {
     }
 
     @Test
+    fun `success state turns off loading`() = coroutineRule.dispatcher.runBlockingTest {
+        val mockApod = Apod("2000-01-01", "apod", "testing",
+            "jpg", "v1", "https://example.com",
+            "https://example.com/hd")
+        val mockDataSource = mock(ApodDataSource::class.java)
+        `when`(mockDataSource.getApod(testDate)).thenReturn(flowOf(Response.success(mockApod)))
+        val apodRepo = ApodRepository(mockDataSource)
+        viewModel = TodayViewModel.TodayViewModelFactory(apodRepo, testDate).create(TodayViewModel::class.java)
+        assertTrue(viewModel.loading.value == false)
+    }
+
+    @Test
     fun `error state hit`() = coroutineRule.dispatcher.runBlockingTest {
         val mockDataSource = mock(ApodDataSource::class.java)
         `when`(mockDataSource.getApod(testDate)).thenReturn(flowOf(Response.error(500, "Error".toResponseBody())))
         val apodRepo = ApodRepository(mockDataSource)
         viewModel = TodayViewModel.TodayViewModelFactory(apodRepo, testDate).create(TodayViewModel::class.java)
         assertEquals(viewModel.error.value, "Error getting apod with status 500.")
+    }
+
+    @Test
+    fun `error state turns off loading`() = coroutineRule.dispatcher.runBlockingTest {
+        val mockDataSource = mock(ApodDataSource::class.java)
+        `when`(mockDataSource.getApod(testDate)).thenReturn(flowOf(Response.error(500, "Error".toResponseBody())))
+        val apodRepo = ApodRepository(mockDataSource)
+        viewModel = TodayViewModel.TodayViewModelFactory(apodRepo, testDate).create(TodayViewModel::class.java)
+        assertTrue(viewModel.loading.value == false)
     }
 }
