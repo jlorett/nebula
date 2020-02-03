5f5ba13397455c495ff10ce021911286904fe926
jal <joshlorett@gmail.com>
2020-02-02 22:25:01 -0500

    Add collapsing toolbarlayout.

--- app/src/main/java/com/joshualorett/nebula/today/TodayPhotoFragment.kt
+++ app/src/main/java/com/joshualorett/nebula/today/TodayPhotoFragment.kt
@@ -9,6 +9,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.doOnPreDraw
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -52,6 +53,8 @@ class TodayPhotoFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        todayCollapsingToolbar.setExpandedTitleColor(ContextCompat.getColor(requireContext(), android.R.color.transparent))
 
         val appCompatActivity = requireActivity() as AppCompatActivity
         appCompatActivity.setSupportActionBar(pictureToolbar)
