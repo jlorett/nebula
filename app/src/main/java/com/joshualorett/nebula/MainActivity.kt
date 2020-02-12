445d716b66a3f822efe74eb19a7710bff88161d1
jal <joshlorett@gmail.com>
2020-02-12 18:20:12 -0500

    Move setting alarm into function.

--- app/src/main/java/com/joshualorett/nebula/MainActivity.kt
+++ app/src/main/java/com/joshualorett/nebula/MainActivity.kt
@@ -15,24 +15,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         if(savedInstanceState == null) {
-            val requestCode = 9
-            val syncIntent = Intent(this, TodaySyncBroadcastReceiver::class.java)
-            val pendingSyncIntent = PendingIntent.getBroadcast(this, requestCode, syncIntent, 0)
-            val syncTime = Calendar.getInstance().apply {
-                timeInMillis = System.currentTimeMillis()
-                set(Calendar.HOUR_OF_DAY, 1)
-                set(Calendar.MINUTE, 0)
-                set(Calendar.SECOND, 0)
-                set(Calendar.MILLISECOND, 0)
-                add(Calendar.DAY_OF_MONTH, 1)
-            }.timeInMillis
-            val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
-            alarmManager.setInexactRepeating(
-                AlarmManager.RTC,
-                syncTime,
-                AlarmManager.INTERVAL_DAY,
-                pendingSyncIntent
-            )
+            setRecurringSyncAlarm()
         }
+    }
+
+    private fun setRecurringSyncAlarm() {
+        val requestCode = 9
+        val syncIntent = Intent(this, TodaySyncBroadcastReceiver::class.java)
+        val pendingSyncIntent = PendingIntent.getBroadcast(this, requestCode, syncIntent, 0)
+        val syncTime = Calendar.getInstance().apply {
+            timeInMillis = System.currentTimeMillis()
+            set(Calendar.HOUR_OF_DAY, 1)
+            set(Calendar.MINUTE, 0)
+            set(Calendar.SECOND, 0)
+            set(Calendar.MILLISECOND, 0)
+            add(Calendar.DAY_OF_MONTH, 1)
+        }.timeInMillis
+        val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        alarmManager.setInexactRepeating(
+            AlarmManager.RTC,
+            syncTime,
+            AlarmManager.INTERVAL_DAY,
+            pendingSyncIntent
+        )
     }
 }
