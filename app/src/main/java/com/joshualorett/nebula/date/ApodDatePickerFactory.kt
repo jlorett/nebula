3e43af300d19a6feed72a609e1c6c9a7f0b66166
jal <joshlorett@gmail.com>
2020-05-30 16:15:06 -0400

    Add DatePicker factory and validator for apods

---
+++ app/src/main/java/com/joshualorett/nebula/date/ApodDatePickerFactory.kt
@@ -0,0 +1,29 @@
+package com.joshualorett.nebula.date
+
+import com.google.android.material.datepicker.CalendarConstraints
+import com.google.android.material.datepicker.MaterialDatePicker
+import java.time.*
+
+/**
+ * Creates a [MaterialDatePicker] to select an APOD submission.
+ * Created by Joshua on 5/30/2020.
+ */
+object ApodDatePickerFactory {
+    fun create(selectionDate: LocalDate): MaterialDatePicker<Long> {
+        val zoneId = ZoneId.ofOffset("UTC", ZoneOffset.UTC)
+        val todayEpochMilli = LocalDate.now().atStartOfDay(zoneId).toInstant().toEpochMilli()
+        val selectionDateEpochMilli = selectionDate.atStartOfDay(zoneId).toInstant().toEpochMilli()
+        val earliestDateEpochMilli = LocalDate.of(1995, 6, 16).atStartOfDay(zoneId).toInstant().toEpochMilli()
+        val calConstraints = CalendarConstraints.Builder()
+            .setOpenAt(selectionDateEpochMilli)
+            .setStart(earliestDateEpochMilli)
+            .setEnd(todayEpochMilli)
+            .setValidator(ApodDateValidator())
+            .build()
+        return MaterialDatePicker.Builder.datePicker()
+            .setSelection(selectionDateEpochMilli)
+            .setInputMode(MaterialDatePicker.INPUT_MODE_CALENDAR)
+            .setCalendarConstraints(calConstraints)
+            .build()
+    }
+}
