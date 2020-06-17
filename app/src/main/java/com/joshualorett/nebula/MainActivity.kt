a97aae1c783da6e0c4c51d34f569ad35711f0994
jal <joshlorett@gmail.com>
2020-06-16 20:10:08 -0400

    Add entry point annotation to MainActivity
    Since we plan on annotation the fragments, we need to annotate any
    activity that uses them.

--- app/src/main/java/com/joshualorett/nebula/MainActivity.kt
+++ app/src/main/java/com/joshualorett/nebula/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceManager
 import com.joshualorett.nebula.today.TodaySyncManager
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
