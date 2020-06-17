8ce861318f904c67b3836c8665f1c177141bb4f2
jal <joshlorett@gmail.com>
2020-06-16 20:07:18 -0400

    Add NebulaApplication for Hilt code generation

---
+++ app/src/main/java/com/joshualorett/nebula/NebulaApplication.kt
@@ -0,0 +1,12 @@
+package com.joshualorett.nebula
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Application for the app.
+ * Created by Joshua on 6/16/2020.
+ */
+@HiltAndroidApp
+class NebulaApplication: Application() {
+}
