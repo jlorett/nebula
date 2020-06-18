e23e4eace3a54fe466237285b15b8481d369fd11
jal <joshlorett@gmail.com>
2020-06-17 21:19:51 -0400

    Add ApodDaoModule to inject the ApodDao

---
+++ app/src/main/java/com/joshualorett/nebula/di/ApodDaoModule.kt
@@ -0,0 +1,34 @@
+package com.joshualorett.nebula.di
+
+import android.content.Context
+import androidx.room.Room
+import com.joshualorett.nebula.apod.database.ApodDao
+import com.joshualorett.nebula.apod.database.ApodDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Singleton
+
+/**
+ * DI Module for database.
+ * Created by Joshua on 6/17/2020.
+ */
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object ApodDaoModule {
+    @Provides
+    @Singleton
+    fun provideDatabase(@ApplicationContext applicationContext: Context): ApodDatabase {
+        return  Room.databaseBuilder(applicationContext,
+            ApodDatabase::class.java,
+            "apod").build()
+    }
+
+    @Provides
+    fun provideDao(database: ApodDatabase): ApodDao {
+        return database.apodDao()
+    }
+}
