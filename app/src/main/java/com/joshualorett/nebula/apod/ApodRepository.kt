d6232c702795b73ab7bcb01001bf40f1ce4966ce
jal <joshlorett@gmail.com>
2020-06-21 19:38:13 -0400

    Inject ApodRepository

--- app/src/main/java/com/joshualorett/nebula/apod/ApodRepository.kt
+++ app/src/main/java/com/joshualorett/nebula/apod/ApodRepository.kt
@@ -6,12 +6,13 @@ import com.joshualorett.nebula.shared.ImageCache
 import com.joshualorett.nebula.shared.Resource
 import java.io.IOException
 import java.time.LocalDate
+import javax.inject.Inject
 
 /**
  * Single point of access to fetch an [Apod] from the ui.
  * Created by Joshua on 1/8/2020.
  */
-class ApodRepository(private val apodDataSource: ApodDataSource, private val apodDao: ApodDao,
+class ApodRepository @Inject constructor(private val apodDataSource: ApodDataSource, private val apodDao: ApodDao,
                      private val imageCache: ImageCache) {
     // The first APOD was 1995-06-16.
     private val earliestDate: LocalDate = LocalDate.of(1995, 6, 16)
