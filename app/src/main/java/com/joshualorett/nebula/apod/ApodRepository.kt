1882cca259ac034756c0dc07f165ec2b3745c74f
jal <joshlorett@gmail.com>
2020-08-30 15:11:49 -0400

    Convert ApodRepository to use Flow

--- app/src/main/java/com/joshualorett/nebula/apod/ApodRepository.kt
+++ app/src/main/java/com/joshualorett/nebula/apod/ApodRepository.kt
@@ -4,7 +4,7 @@ import com.joshualorett.nebula.apod.api.ApodDataSource
 import com.joshualorett.nebula.apod.database.ApodDao
 import com.joshualorett.nebula.shared.ImageCache
 import com.joshualorett.nebula.shared.Resource
-import java.io.IOException
+import kotlinx.coroutines.flow.*
 import java.time.LocalDate
 import javax.inject.Inject
 
@@ -12,8 +12,9 @@ import javax.inject.Inject
  * Single point of access to fetch an [Apod] from the ui.
  * Created by Joshua on 1/8/2020.
  */
-class ApodRepository @Inject constructor(private val apodDataSource: ApodDataSource, private val apodDao: ApodDao,
-                     private val imageCache: ImageCache) {
+class ApodRepository @Inject constructor(private val apodDataSource: ApodDataSource,
+                                         private val apodDao: ApodDao,
+                                         private val imageCache: ImageCache) {
     // The first APOD was 1995-06-16.
     private val earliestDate: LocalDate = LocalDate.of(1995, 6, 16)
 
@@ -21,24 +22,27 @@ class ApodRepository @Inject constructor(private val apodDataSource: ApodDataSou
      * Fetch [Apod] by date. This will attempt to get it from the database first and if not found,
      * will fetch from the api.
      */
-    suspend fun getApod(date: LocalDate): Resource<Apod, String> {
+    fun getApod(date: LocalDate): Flow<Resource<Apod, String>> {
         if (date.isBefore(earliestDate)) {
-            return Resource.Error("Date can't be before 1995-06-16.")
+            return flowOf(Resource.Error("Date can't be before 1995-06-16."))
         }
-        val cachedApod = apodDao.loadByDate(date.toString())?.toApod()
-        return if (cachedApod == null) {
-            getApodByDataSource(date)
-        } else {
-            Resource.Success(cachedApod)
-        }
+        return apodDao.loadByDate(date.toString())
+            .flatMapLatest { entity ->
+                val cachedApod = entity?.toApod()
+                if (cachedApod == null) {
+                    getApodByDataSource(date)
+                } else {
+                    flowOf(Resource.Success(cachedApod))
+                }
+            }
     }
 
     /***
      * Fetch a fresh [Apod] by date. This will always attempt to fetch the apod from the api.
      */
-    suspend fun getFreshApod(date: LocalDate): Resource<Apod, String> {
+    fun getFreshApod(date: LocalDate): Flow<Resource<Apod, String>> {
         if (date.isBefore(earliestDate)) {
-            return Resource.Error("Date can't be before 1995-06-16.")
+            return flowOf(Resource.Error("Date can't be before 1995-06-16."))
         }
         return getApodByDataSource(date)
     }
@@ -46,42 +50,39 @@ class ApodRepository @Inject constructor(private val apodDataSource: ApodDataSou
     /***
      * This will fetch an [Apod] from the database by its id.
      */
-    suspend fun getCachedApod(id: Long): Resource<Apod, String> {
-        val cachedApod = apodDao.loadById(id)?.toApod()
-        return if (cachedApod == null) {
-            Resource.Error("Apod not found in database.")
-        } else {
-            Resource.Success(cachedApod)
-        }
+    fun getCachedApod(id: Long): Flow<Resource<Apod, String>> {
+        return apodDao.loadById(id)
+            .map { entity ->
+                val cachedApod = entity?.toApod()
+                if (cachedApod == null) {
+                    Resource.Error("Apod not found in database.")
+                } else {
+                    Resource.Success(cachedApod)
+                }
+            }
     }
 
-    private suspend fun getApodByDataSource(date: LocalDate): Resource<Apod, String> {
-        try {
-            val response = apodDataSource.getApod(date)
-            return if (response.isSuccessful) {
+    private fun getApodByDataSource(date: LocalDate): Flow<Resource<Apod, String>> {
+        return apodDataSource.getApod(date).flatMapLatest { response ->
+            if (response.isSuccessful) {
                 val networkApod = response.body()?.toApod()
                 if (networkApod == null) {
-                    Resource.Error("Empty network body.")
+                    flowOf(Resource.Error("Empty network body."))
                 } else {
-                    return cacheApod(networkApod)
+                    cacheApod(networkApod)
                 }
             } else {
-                Resource.Error("Error getting apod with status ${response.code()}.")
+                flowOf(Resource.Error("Error getting apod with status ${response.code()}."))
             }
-        } catch (e: IOException) {
-            return Resource.Error("Your network is unavailable. Check your data or wifi connection.")
+        }.catch {
+            emit(Resource.Error("Your network is unavailable. Check your data or wifi connection."))
         }
     }
 
-    private suspend fun cacheApod(apod: Apod): Resource<Apod, String> {
+    private suspend fun cacheApod(apod: Apod): Flow<Resource<Apod, String>> {
         apodDao.delete(apod.toEntity())
         val id = apodDao.insertApod(apod.toEntity())
-        val cachedApod = apodDao.loadById(id)?.toApod()
-        return if (cachedApod == null) {
-            Resource.Error("Error loading apod from database.")
-        } else {
-            Resource.Success(cachedApod)
-        }
+        return getCachedApod(id)
     }
 
     /***
