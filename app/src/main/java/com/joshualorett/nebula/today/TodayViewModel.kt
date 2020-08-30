e70325e20506123e00f4ac39069c76c74f71ba9c
jal <joshlorett@gmail.com>
2020-08-30 15:14:18 -0400

    Update TodayViewModel to work with Flow

--- app/src/main/java/com/joshualorett/nebula/today/TodayViewModel.kt
+++ app/src/main/java/com/joshualorett/nebula/today/TodayViewModel.kt
@@ -1,16 +1,15 @@
 package com.joshualorett.nebula.today
 
 import androidx.hilt.lifecycle.ViewModelInject
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import com.joshualorett.nebula.apod.Apod
 import com.joshualorett.nebula.apod.ApodRepository
 import com.joshualorett.nebula.shared.OneShotEvent
 import com.joshualorett.nebula.shared.Resource
 import com.joshualorett.nebula.shared.data
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.*
 import java.time.LocalDate
 
 /**
@@ -18,8 +17,30 @@ import java.time.LocalDate
  * Created by Joshua on 1/11/2020.
  */
 class TodayViewModel @ViewModelInject constructor(private val apodRepository: ApodRepository): ViewModel() {
-    private val _apod: MutableLiveData<Resource<Apod, String>> = MutableLiveData()
-    val apod: LiveData<Resource<Apod, String>> = _apod
+    private var currentApod: Apod? = null
+    private val today = LocalDate.now()
+    private val _date: MutableLiveData<LocalDate> = MutableLiveData(today)
+    private var refresh = false
+    var bgDispatcher: CoroutineDispatcher = Dispatchers.IO
+
+    val apod = _date.asFlow()
+        .filter { date -> date != null }
+        .flatMapLatest {  date ->
+            if(refresh) {
+                refresh = false
+                apodRepository.getFreshApod(date)
+            } else {
+                apodRepository.getApod(date)
+            }
+        }
+        .onEach { res ->
+            currentApod = res.data
+        }
+        .onStart { emit(Resource.Loading) }
+        .catch { throwable -> emit(Resource.Error("Couldn't fetch apod.")) }
+        .flowOn(bgDispatcher)
+        .conflate()
+        .asLiveData()
 
     private val _navigateVideoLink: MutableLiveData<OneShotEvent<String?>> = MutableLiveData()
     val navigateVideoLink: LiveData<OneShotEvent<String?>> = _navigateVideoLink
@@ -27,44 +48,32 @@ class TodayViewModel @ViewModelInject constructor(private val apodRepository: Ap
     private val _navigateFullPicture: MutableLiveData<OneShotEvent<Long>> = MutableLiveData()
     val navigateFullPicture: LiveData<OneShotEvent<Long>> = _navigateFullPicture
 
-    private val today = LocalDate.now()
-
-    private val _date: MutableLiveData<LocalDate> = MutableLiveData()
-
-    init {
-        updateDate(_date.value ?: today)
-    }
-
     fun currentDate(): LocalDate? {
         return _date.value
     }
 
     fun videoLinkClicked() {
-        val currentApod = _apod.value?.data
-        if(currentApod?.mediaType == "video") {
-            _navigateVideoLink.value = OneShotEvent(currentApod.url)
+        currentApod?.let {
+            if(it.mediaType == "video") {
+                _navigateVideoLink.value = OneShotEvent(it.url)
+            }
         }
     }
 
     fun onPhotoClicked() {
-        val apod = _apod.value?.data
-        if(apod != null) {
-            _navigateFullPicture.value = OneShotEvent(apod.id)
+        currentApod?.let {
+            _navigateFullPicture.value = OneShotEvent(it.id)
         }
     }
 
     fun updateDate(date: LocalDate) {
         _date.value = date
-        viewModelScope.launch {
-            _apod.value = Resource.Loading
-            _apod.value = apodRepository.getApod(date)
-        }
     }
 
     fun refresh() {
-        viewModelScope.launch {
-            _apod.value = Resource.Loading
-            _apod.value = apodRepository.getFreshApod(_date.value ?: today)
+        refresh = true
+        _date.value?.let {
+            updateDate(it)
         }
     }
 }
