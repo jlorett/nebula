3ef0970566bfdfaa27ed9e698e1afed4fd98b16c
jal <joshlorett@gmail.com>
2021-01-24 17:45:39 -0500

    Replace TestCoroutineRule with MainCoroutineRule

---
+++ app/src/test/java/com/joshualorett/nebula/MainCoroutineRule.kt
@@ -0,0 +1,35 @@
+package com.joshualorett.nebula
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.TestCoroutineScope
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
+
+/**
+ *  Sets the main [CoroutineDispatcher] to a [TestCoroutineScope] for unit testing.
+ *  [TestCoroutineScope.cleanupTestCoroutines] is called after execution of each test,
+ * and will cause an exception if any coroutines are leaked.
+ *
+ * Created by Joshua on 1/12/2020.
+ */
+
+@ExperimentalCoroutinesApi
+class MainCoroutineRule(val dispatcher: TestCoroutineDispatcher = TestCoroutineDispatcher()):
+    TestWatcher(),
+    TestCoroutineScope by TestCoroutineScope(dispatcher) {
+    override fun starting(description: Description?) {
+        super.starting(description)
+        Dispatchers.setMain(dispatcher)
+    }
+
+    override fun finished(description: Description?) {
+        super.finished(description)
+        cleanupTestCoroutines()
+        Dispatchers.resetMain()
+    }
+}
