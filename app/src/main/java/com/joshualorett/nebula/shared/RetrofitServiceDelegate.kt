3a2cc1bddb4aad380969effdd791d9ab531fd9ac
jal <joshlorett@gmail.com>
2021-03-22 22:05:38 -0400

    Remove uneeded service delegate and retrofit client

--- app/src/main/java/com/joshualorett/nebula/shared/RetrofitServiceDelegate.kt
+++
@@ -1,9 +0,0 @@
-package com.joshualorett.nebula.shared
-
-/**
- * Delegates service creation to retrofit instance.
- * Created by Joshua on 1/6/2020.
- */
-interface RetrofitServiceDelegate {
-    fun <T> create(key: String, service: Class<T>): T
-}
