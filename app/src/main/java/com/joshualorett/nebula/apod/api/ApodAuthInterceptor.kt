e9a236c2432458c061588e0a3181cbbd16b023d5
jal <joshlorett@gmail.com>
2021-03-22 21:43:34 -0400

    Add ApodAuthInterceptor to attach api key to network requests

---
+++ app/src/main/java/com/joshualorett/nebula/apod/api/ApodAuthInterceptor.kt
@@ -0,0 +1,20 @@
+package com.joshualorett.nebula.apod.api
+
+import okhttp3.Interceptor
+import okhttp3.Response
+
+/**
+ * Attaches api key before making Apod network requests.
+ * Created by Joshua on 3/22/2021.
+ */
+class ApodAuthInterceptor(private val key: String): Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        var request = chain.request()
+        val url = request.url
+            .newBuilder()
+            .addQueryParameter("api_key", key)
+            .build()
+        request = request.newBuilder().url(url).build()
+        return chain.proceed(request)
+    }
+}
